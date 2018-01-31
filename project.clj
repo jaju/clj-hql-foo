32fd0f1068c9f81a9dedc7a645f028ea6631ae12
Ravindra R. Jaju <jaju@msync.org>
2018-01-31 22:08:50 +0530

    Dep updates.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/jaju/clj-hql-foo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
-                 [org.apache.hive/hive-exec "2.3.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.apache.hive/hive-exec "2.3.2"]
                  [cheshire "5.8.0"]]
 
   :main msync.hql-foo
